9343cff45ca9cd079c26a649b0e960754501605e
geniusgo <gogenius28@naver.com>
2024-11-30 13:38:34 +0900

    board 턴 진행 동작 완료

--- src/App.tsx
+++ src/App.tsx
@@ -14,17 +14,16 @@ const reducer = (state: Histories, action: Action) => {
   }
 };
 
-// 컴포넌트가 깊지 않기 때문에 props로 상태 전달하고 관리
+// 컴포넌트 구조가 깊지 않기 때문에 props로 상태 전달하고 관리
 function App() {
-  const initState: Histories = [Array(9).fill('')];
-  const [histories, dispatch] = useReducer(reducer, initState); // 진행된 턴의 누적 기록 보관
+  const [histories, dispatch] = useReducer(reducer, [Array(9).fill('')]); // 진행된 턴의 누적 기록 보관
   const [turn, setTurn] = useState('O'); // 누구 차례인지, 처음은 O부터 시작
 
   return (
     <div>
-      <Header turn='O' />
+      <Header turn={turn} />
       <div className='contents'>
-        <Board />
+        <Board turn={turn} setTurn={setTurn} />
         <Sidebar />
       </div>
     </div>
--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -1,26 +1,57 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './Board.css';
 import { Record } from '../types/types';
 
-const Board = () => {
-  const [record, setRecord] = useState<Record | null>(null);
+interface Props {
+  turn: string;
+  setTurn: React.Dispatch<React.SetStateAction<string>>;
+}
+
+const Board = ({ turn, setTurn }: Props) => {
+  const [records, setRecords] = useState<Record>(Array(9).fill(''));
+
+  // space 클릭할 때마다 O-X 화면에 표시하고 records에 기록
+  const handleClick = (e: React.MouseEvent<HTMLTableElement>) => {
+    const target = e.target as HTMLElement; // dataset 가져오기 위해 e.target을 HTMLElement 타입으로 단언
+    records[Number(target.dataset.space)] = turn;
+    setRecords([...records]);
+    setTurn(turn === 'O' ? 'X' : 'O');
+  };
 
   return (
-    <table className='board'>
+    <table className='board' onClick={handleClick}>
       <tr className='board-row'>
-        <td className='space'>0</td>
-        <td className='space'>1</td>
-        <td className='space'>2</td>
+        <td className='space' data-space='0'>
+          {records[0]}
+        </td>
+        <td className='space' data-space='1'>
+          {records[1]}
+        </td>
+        <td className='space' data-space='2'>
+          {records[2]}
+        </td>
       </tr>
       <tr className='board-row'>
-        <td className='space'>3</td>
-        <td className='space'>4</td>
-        <td className='space'>5</td>
+        <td className='space' data-space='3'>
+          {records[3]}
+        </td>
+        <td className='space' data-space='4'>
+          {records[4]}
+        </td>
+        <td className='space' data-space='5'>
+          {records[5]}
+        </td>
       </tr>
       <tr className='board-row'>
-        <td className='space'>6</td>
-        <td className='space'>7</td>
-        <td className='space'>8</td>
+        <td className='space' data-space='6'>
+          {records[6]}
+        </td>
+        <td className='space' data-space='7'>
+          {records[7]}
+        </td>
+        <td className='space' data-space='8'>
+          {records[8]}
+        </td>
       </tr>
     </table>
   );
